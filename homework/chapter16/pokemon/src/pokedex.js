3c28d0f3a5cec602fc8d6cdde71436d3b6b1c358
pongos <mimousteileis@gmail.com>
2016-08-03 18:36:25 +0300

    add chapter 16

---
+++ homework/chapter16/pokemon/src/pokedex.js
@@ -0,0 +1,75 @@
+export let pokedex = {
+  categories: [{
+      name: 'Fire',
+      pokemons: [{
+          level: '06',
+          name: 'Charmander',
+          hungry: true
+        },
+        {
+          level: '12',
+          name: 'Vulpix',
+          hungry: true
+        },
+        {
+          level: '10',
+          name: 'Magmar',
+          hungry: true
+        },
+        {
+          level: '22',
+          name: 'Cyndaquil',
+          hungry: true
+        }
+      ]
+    },
+    {
+      name: 'Water',
+      pokemons: [{
+          level: '03',
+          name: 'Squirtle',
+          hungry: true
+        },
+        {
+          level: '45',
+          name: 'Blastoise',
+          hungry: true
+        },
+        {
+          level: '09',
+          name: 'Staryu',
+          hungry: true
+        },
+        {
+          level: '26',
+          name: 'Gyarados',
+          hungry: true
+        },
+        {
+          level: '29',
+          name: 'Tentacruel',
+          hungry: true
+        }
+      ]
+    },
+    {
+      name: 'Electric',
+      pokemons: [{
+          level: '74',
+          name: 'Pikachu',
+          hungry: true
+        },
+        {
+          level: '34',
+          name: 'Electabuzz',
+          hungry: true
+        },
+        {
+          level: '88',
+          name: 'Zapdos',
+          hungry: true
+        }
+      ]
+    },
+  ]
+}
